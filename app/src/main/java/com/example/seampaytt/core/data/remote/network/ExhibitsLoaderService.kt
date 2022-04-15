06f7b5f846763d1fae62c127ce8f4371e44aae23
timmyCoder <olatoyedan@gmail.com>
2022-04-15 18:08:20 +0100

    added exhibit class and response

--- app/src/main/java/com/example/seampaytt/core/data/remote/network/ExhibitsLoaderService.kt
+++ app/src/main/java/com/example/seampaytt/core/data/remote/network/ExhibitsLoaderService.kt
@@ -1,11 +1,12 @@
 package com.example.seampaytt.core.data.remote.network
 
+import com.example.seampaytt.core.data.remote.response.Exhibit
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ExhibitsLoaderService {
 
     @GET("list")
-    suspend fun getExhibitList() : Unit
+    suspend fun getExhibitList() : List<Exhibit>
 
 }
---
+++ app/src/main/java/com/example/seampaytt/core/data/remote/response/ExhibitResponse.kt
@@ -0,0 +1,8 @@
+package com.example.seampaytt.core.data.remote.response
+
+import com.squareup.moshi.Json
+
+data class Exhibit(
+    @field:Json(name = "title") var title: String? = null,
+    @field:Json(name = "images") var images: ArrayList<String> = arrayListOf()
+)
