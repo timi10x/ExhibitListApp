1a88d011852ca553569bf0dfef3a71ef4bf9c065
timmyCoder <olatoyedan@gmail.com>
2022-04-17 00:32:21 +0100

    removed unused code

--- app/src/main/java/com/example/seampaytt/core/data/remote/response/ExhibitResponse.kt
+++ app/src/main/java/com/example/seampaytt/core/data/remote/response/ExhibitResponse.kt
@@ -6,7 +6,3 @@ data class Exhibit(
     @field:Json(name = "title") var title: String? = null,
     @field:Json(name = "images") var images: List<String> = arrayListOf()
 )
-
-data class ExhibitApiResponse(
-    val exhibits: List<Exhibit>
-)
