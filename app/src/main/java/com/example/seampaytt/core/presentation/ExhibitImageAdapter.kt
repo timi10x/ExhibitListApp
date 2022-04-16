03a20e903b7c1e5a5ff8cfd408f59ef687b571d2
timmyCoder <olatoyedan@gmail.com>
2022-04-16 15:40:29 +0100

    added exhibit image adapter

---
+++ app/src/main/java/com/example/seampaytt/core/presentation/ExhibitImageAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.seampaytt.core.presentation
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.seampaytt.R
+import com.example.seampaytt.core.data.local.entity.ImagesEntity
+
+
+class ExhibitImageAdapter(
+    private val context: Context,
+    private val exhibits: ArrayList<ImagesEntity>
+) :
+    RecyclerView.Adapter<ExhibitImageAdapter.CustomViewHolder?>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+
+    override fun getItemCount(): Int = exhibits.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val view: View = inflater.inflate(R.layout.exhibit_image_item, parent, false)
+        return CustomViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        val exhibit: ImagesEntity = exhibits[position]
+        holder.tvChapterName.text = "exhibit"
+        Glide.with(context)
+            .load(exhibit.images)
+            .centerInside()
+            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+            .into(holder.exhibitImage)
+    }
+
+
+    inner class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var exhibitImage: ImageView = itemView.findViewById<View>(R.id.exhibitImage) as ImageView
+        var tvChapterName: TextView = itemView.findViewById<View>(R.id.tvExhibitImageName) as TextView
+
+    }
+
+}
